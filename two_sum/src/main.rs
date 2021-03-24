baa64072d40ea503e6069272238db4b4cc5fb1f3
natsuokawai <kawai.natsuo@gmail.com>
2021-03-25 08:50:13 +0900

    Use HashMap

--- two_sum/src/main.rs
+++ two_sum/src/main.rs
@@ -1,22 +1,22 @@
+use std::collections::HashMap;
+
 fn main() {
-	Solution::two_sum(vec![2, 7, 11, 15], 9);
+    assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
 }
 
 struct Solution;
 
 impl Solution {
-    fn two_sum(nums: Vec::<i32>, target: i32) -> Vec::<i32> {
-        let mut result = Vec::new();
+    fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut num_hash: HashMap<i32, i32> = HashMap::new();
 
-        for (i, num_i) in nums.iter().enumerate() {
-            for (j, num_j) in nums[i+1..].iter().enumerate() {
-                if num_i + num_j == target {
-                    result = vec![i as i32, (i + j + 1) as i32];
-                }
-            }
+        for (i, num) in nums.iter().enumerate() {
+            match num_hash.get(num) {
+                Some(n) => return vec![*n, i as i32],
+                None => num_hash.insert(target - num, i as i32),
+            };
         }
-        
-        result
+
+        vec![]
     }
 }
-
