1aead8048ee0b73bf0d764d92be84bab63a615cb
natsuokawai <kawai.natsuo@gmail.com>
2021-03-29 09:21:11 +0900

    Improve longest_substring_without_repeating_characters

--- longest_substring_without_repeating_characters/src/main.rs
+++ longest_substring_without_repeating_characters/src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::cmp;
+
 fn main() {
     assert_eq!(
         Solution::length_of_longest_substring(String::from("pwwkew")),
@@ -9,23 +12,21 @@ struct Solution;
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut map = HashMap::new();
         let mut max_len = 0;
+        let mut current = 0;
+        let mut before = -1;
 
-        for i in 0..s.len() {
-            let mut substr: Vec<char> = vec![];
-            for c in (&s[i..]).chars() {
-                if substr.contains(&c) {
-                    break;
-                } else {
-                    substr.push(c);
-                }
-            }
-
-            if substr.len() > max_len {
-                max_len = substr.len();
-            }
+        for c in s.chars() {
+            // if map already contains 'c', returns None
+            // update 'before' as base pointer
+            if let Some(last) = map.insert(c, current) {
+                before = cmp::max(before, last);
+            } 
+            max_len = cmp::max(max_len, current - before);
+            current += 1;
         }
 
-        max_len as i32
+        max_len
     }
 }
