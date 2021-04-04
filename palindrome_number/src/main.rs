2b65abd394948b7dbda282e07102e661f4ea6b4d
natsuokawai <kawai.natsuo@gmail.com>
2021-04-05 08:42:57 +0900

    Modify palindrome_number

--- palindrome_number/src/main.rs
+++ palindrome_number/src/main.rs
@@ -9,25 +9,17 @@ struct Solution;
 
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
-        if x < 0 {
+        if x < 0 || x % 10 == 0 {
             return false;
         }
-        if x < 10 {
-            return true;
-        }
 
-        let mut arr = Vec::new();
         let mut num = x;
+        let mut reverse = 0;
         while num != 0 {
-            arr.push(num % 10);
+            reverse = reverse * 10 + num % 10;
             num /= 10;
         }
 
-        for (i, n) in arr.iter().rev().enumerate() {
-            if &arr[i] != n {
-                return false;
-            }
-        }
-        true
+        reverse == x
     }
 }
